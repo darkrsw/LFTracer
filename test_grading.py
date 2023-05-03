147b0946e860d82a54bb47940855184c013c8a8b
Dongsun Kim <darkrsw@gmail.com>
2023-05-03 13:47:11 +0900

    added grading tests.

--- test_grading.py
+++ test_grading.py
@@ -1,5 +1,8 @@
 from LFTracer import LFTracer
 import nayajson
+from divide import divide
+from merge_insertion_sort import merge_insertion_sort
+from red_black_tree import RBTree, RBNode
 
 
 def test_lvv_json1():
@@ -75,3 +78,38 @@ def test_lvv_json3():
 
     answer = {'parse': {337: 1, 338: 1, 339: 1, 340: 1, 342: 1, 343: 2, 344: 1, 345: 2}, 'process_char': {49: 624, 51: 624, 52: 624, 53: 624, 54: 624, 55: 303, 58: 297, 61: 291, 64: 290, 67: 289, 70: 279, 73: 264, 75: 238, 78: 238, 81: 238, 84: 238, 86: 238, 88: 238, 90: 238, 310: 624, 313: 1248, 56: 6, 57: 6, 74: 26, 92: 321, 108: 321, 122: 321, 131: 321, 137: 321, 147: 321, 160: 321, 166: 321, 171: 321, 176: 321, 181: 321, 188: 321, 193: 321, 198: 321, 205: 321, 210: 321, 215: 321, 222: 321, 223: 295, 227: 269, 230: 269, 311: 269, 224: 26, 225: 26, 226: 26, 231: 26, 232: 26, 233: 26, 234: 26, 71: 15, 72: 15, 68: 10, 69: 10, 62: 1, 63: 1, 65: 1, 66: 1, 59: 6, 60: 6}}
     assert traced.getLFMap() == answer
+
+
+def test_divide():
+    with LFTracer(target_func = ["divide"]) as traced:
+        divide(5, 2)
+
+    answer = {'divide': {6: 1, 9: 1, 12: 1, 14: 1, 15: 1, 16: 1, 18: 1, 21: 1, 28: 1, 30: 2, 32: 1, 33: 1, 34: 1, 39: 1, 41: 1, 42: 1, 43: 1, 44: 1, 45: 1, 48: 1, 50: 2}}
+    assert traced.getLFMap() == answer
+
+
+def test_red_black_tree():
+    with LFTracer(target_func=["left_rotate", "right_rotate",  "insert"]) as traced:
+        rb = RBTree()
+        children = [11, 2, 14, 1, 7, 15, 5, 8, 4]
+        for child in children:
+            node = RBNode(child, 1)
+            rb.insert(node)
+
+    answer = {'insert': {62: 9, 64: 9, 65: 9, 67: 27, 74: 9, 75: 9, 77: 1, 83: 9, 84: 9, 85: 9, 87: 18, 68: 18, 69: 18, 72: 10, 78: 8, 80: 4, 70: 8, 82: 4}, 'left_rotate': {20: 1, 22: 1, 23: 1, 27: 1, 28: 1, 29: 1, 30: 1, 32: 1, 34: 1, 35: 1, 38: 1, 39: 2}, 'right_rotate': {41: 1, 43: 1, 44: 1, 48: 1, 49: 1, 50: 1, 51: 1, 53: 1, 54: 1, 59: 1, 60: 2}}
+    assert traced.getLFMap() == answer
+
+def test_merge_insertion_sort():
+    with LFTracer(target_func=["binary_search_insertion", "merge", "sortlist_2d", "merge_insertion_sort"]) as traced:
+        merge_insertion_sort([0, 5, 3, 2, 2])
+
+    answer = {'merge_insertion_sort': {65: 1, 89: 1, 92: 1, 98: 1, 99: 1, 100: 4, 101: 3, 104: 2, 110: 2, 111: 1, 113: 1, 102: 1, 115: 1, 121: 1, 123: 1, 136: 1, 138: 1, 151: 1, 153: 1, 165: 1, 166: 1, 167: 1, 169: 1, 183: 1, 184: 2, 185: 1, 187: 1, 190: 1, 193: 1, 195: 2}, 'sortlist_2d': {53: 3, 58: 3, 59: 3, 61: 1, 62: 2, 60: 4}, 'merge': {39: 1, 44: 1, 45: 2, 46: 1, 47: 1, 50: 2}, 'binary_search_insertion': {18: 2, 23: 2, 24: 2, 25: 4, 26: 4, 27: 4, 31: 2, 34: 1, 28: 2, 29: 2, 30: 2, 35: 2, 36: 4, 32: 1}}
+    assert traced.getLFMap() == answer
+
+if __name__ == '__main__':
+    test_lvv_json1()
+    test_lvv_json2()
+    test_lvv_json3()
+    test_divide()
+    test_red_black_tree()
+    test_merge_insertion_sort()
